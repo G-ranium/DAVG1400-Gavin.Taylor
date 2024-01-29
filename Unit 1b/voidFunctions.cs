ab83b91ee3b1c5891f8b43c112dd28ba4ff8bd2d
Gavin Taylor <10824291@uvu.edu>
2024-01-29 16:11:52 -0700

    With new operations

--- Unit 1b/voidFunctions.cs
+++ Unit 1b/voidFunctions.cs
@@ -8,24 +8,58 @@ namespace Unit_1b
 {
     class Program
     {
-        static void Main(string[] args)
+        public Operations myOperator;
+        public void Main()
         {
+            myOperator = new Operations();
+
+            Console.WriteLine("What's the password?");
+            myOperator.Password(Console.ReadLine());
 
             Console.WriteLine("How much did you pay? ");
             double price = double.Parse(Console.ReadLine());
             Console.WriteLine("How much would you like to tip in %? ");
             double tip = double.Parse(Console.ReadLine());
-            calculateTip(price, tip);
+            myOperator.calculateTip(price, tip);
 
-
-
+            Console.WriteLine("Please enter an integer");
+            int num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Please enter another integer");
+            int num2 = int.Parse(Console.ReadLine());
+            myOperator.Compare(num1, num2);
         }
-
-        public static void calculateTip (double value1, double value2)
+    }
+    public class Operations
+    {
+        public void calculateTip(double value1, double value2)
         {
             var number = value1 * (value2 / 100);
             Console.WriteLine($"The tip is {number} your total will be {(number + value1)}");
             Console.ReadLine();
+        }
+        public void Compare(int value1, int value2)
+        {
+            if (value1 > value2)
+            {
+                Console.WriteLine($"{value1} is greater than {value2}");
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine($"{value2} is greater than {value1}");
+                Console.ReadLine();
+            }
+        }
+        public void Password(string password)
+        {
+            if (password == "openSesame")
+            {
+                Console.WriteLine("Password is correct");
+            }
+            else
+            {
+                Console.WriteLine("C'mon man you aren't supposed to be here...");
+            }
         }
     }
 }
