1fa3d24e5338a76de60c4ed6eae1355a96a1a5bf
Gavin Taylor <10824291@uvu.edu>
2024-03-04 13:26:11 -0700

    Finished challenge

---
+++ Unit 4/Prototype 2/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.UIElements;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    private float timer = 2f;
+    private bool timerOn = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space) & timerOn == false)
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            timerOn = true;
+        }
+
+        if (timerOn == true)
+        {
+            if (timer < 0)
+            {
+                timer = 2f;
+                timerOn = false;
+            }
+            else
+            {
+                timer -= 3 * Time.deltaTime;
+            }
+        }
+    }
+}
