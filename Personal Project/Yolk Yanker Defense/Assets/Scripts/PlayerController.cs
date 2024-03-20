80270a5330740dbc8ff170a871a7dda94e8c2e3c
Gavin Taylor <10824291@uvu.edu>
2024-03-20 12:24:24 -0600

    Player movment added
    In order to get play to follow mouse pointer, it will be a lot more complex. So for now movement is based on horizontal input (either from A and D or the arrow keys)

---
+++ Personal Project/Yolk Yanker Defense/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions.Must;
+
+public class PlayerController : MonoBehaviour
+{
+    public Vector3 horizontalInput;
+    private float turnSpeed = 500f;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //uses input from WASD to rotate character
+        horizontalInput.y = Input.GetAxis("Horizontal");
+        //horizontalInput.y = Input.GetAxis("Mouse X") + Input.GetAxis("Mouse Y");
+        transform.Rotate(horizontalInput * turnSpeed * Time.deltaTime);
+    }
+    
+}
